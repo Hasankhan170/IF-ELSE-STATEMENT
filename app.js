fc34e86e03d6b46d01ee9dcf860ab8151a254943
Hasankhan170 <hasankhanali0316@gmail.com>
2024-02-29 09:15:32 -0800

    index file

---
+++ app.js
@@ -0,0 +1,101 @@
+// IF ELSE STATEMENT Q/A 
+
+// QUESTION.1
+// var number = +prompt("enter a number");
+// if(number% 3 === 0 ){
+//     console.log("this number is divisible by 3")
+// }else{
+//     console.log("number is not divisible by 3")
+// }
+
+// // DONE 
+
+// QUESTION.2
+
+// var number = 101 ;
+// var reminder =(number % 2)
+// if(reminder == 0 ){
+//     console.log(`${number} is even`)
+// }else{
+//     console.log(`${number} is odd`)
+// }
+
+// done 
+
+// QUESTION.3 
+
+// var age = prompt(18);
+
+// if (age >= 18){
+//     console.log("old enough")
+// }else{
+//     console.log("too young")
+// }
+
+// done 
+
+// QUESTION.4
+
+// var userNAME = prompt("ENTER YOU NAME")
+
+// if(userNAME == "hasan"){
+//     console.log("assalam o alaikum")
+// }else{
+//     console.log("no reply")
+// }
+// done 
+
+// QUESTION.6
+
+// var x = prompt('enter name');
+// var y = x.toUpperCase();
+
+// if(x==y){
+//     console.log('uper case')
+// }else{
+//     console.log("lower case")
+// }
+
+// DONE 
+
+
+// QUESTION.10
+
+// var correctPassword = ('hasan');
+// var userPassword = prompt("Please enter your password:");
+// if (userPassword) {
+//  console.log("Please enter your password");
+// } else {
+//   if (userPassword === correctPassword) {
+//     console.log("Correct! The password you entered matches the original password");
+//   } else {
+//     console.log("Incorrect password");
+//   }
+// }
+
+// done or not done 
+
+
+// QUESTION.11
+
+// var firstname = prompt("ALI")
+// if(firstname === "hasan"){
+//     document.write("hello hasan")
+//     console.log("you are hasan")
+// }else{
+//     console.log("you are not hasan")
+// }
+// done 
+
+
+// QUESTION.12
+
+// var greeting= prompt('good day')
+// var hour= 13;
+// if(hour <= 18){
+//     console.log("good day")
+// }else{
+//     console.log("good evening")
+// }
+
+// not done 
